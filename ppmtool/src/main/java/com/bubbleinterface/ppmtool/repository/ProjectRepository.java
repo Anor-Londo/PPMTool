02f00798714ad71810335f8c4b2c9dafe3b2dde2
Michael <Mishareznik@yandex.ru>
2019-02-12 07:11:37 +0300

    project object and repo

---
+++ ppmtool/src/main/java/com/bubbleinterface/ppmtool/repository/ProjectRepository.java
@@ -0,0 +1,9 @@
+package com.bubbleinterface.ppmtool.repository;
+
+import com.bubbleinterface.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+}
